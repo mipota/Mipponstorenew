88738aeee05fa8fec6ec17332ec7bda316178af0
mipota <49098559+mipota@users.noreply.github.com>
2019-05-02 20:33:34 +0900

    pullの練習

--- pic_send.php
+++ pic_send.php
@@ -1,6 +1,7 @@
 <html>
 <body>
 <h1>画像アップ</h1>
+<h2>hello</h2>
 <form action="pic_receive.php" method="post" enctype="multipart/form-data">
 <p><input type="file" name="img"></p>
 <p><input type="submit" value="送信"></p>
